2d0bd6952c4ab3efabcbe327ed18d9484d5144e8
unknown <4450944+rcmx@users.noreply.github.com>
2024-08-03 07:26:24 -0700

    create vim config dir

--- installWin.bat
+++ installWin.bat
@@ -5,4 +5,9 @@ copy windows\powershell\Microsoft.PowerShell_profile.ps1 %HOMEPATH%\Documents\Wi
 
 copy windows\vscode\settings.json %APPDATA%\Code\User\settings.json
 
+set vimConfigDir="%APPDATA%\..\Local\vim"
+if not exist %vimConfigDir% (
+    mkdir "%vimConfigDir%"
+) 
+copy .config\nvim\init.vim %vimConfigDir%\init.vim
 

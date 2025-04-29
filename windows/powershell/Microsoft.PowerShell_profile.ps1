47f578b59929f753aff1c79095dcf1a3a440556a
sv <126982758+svcoderssvwork@users.noreply.github.com>
2025-04-29 13:47:34 -0700

    add lg=lazygit alias

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,6 @@ function gs {git status $args}
 function gss {git status -s}
 function gl {git log --graph --pretty=format:'\''%Cred%h%Creset -%s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\}
 function glol {git log --graph --abbrev-commit --oneline --decorate}
+
+set-alias -name lg -value lazygit
+

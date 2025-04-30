6f6f5533b6a4737752629127621ed33b6ddd7018
rcmx <4450944+rcmx@users.noreply.github.com>
2025-04-29 19:24:32 -0700

    use built-in lsp keymaps

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -34,12 +34,6 @@ return {
                     }
                 }
             })
-
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
         end
     },
 }

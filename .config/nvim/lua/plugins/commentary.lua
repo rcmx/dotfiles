0d030e2e90c5105c490f39b273520c310819e19d
sv <126982758+svcoderssvwork@users.noreply.github.com>
2025-04-30 14:20:21 -0700

    add commentary

---
+++ .config/nvim/lua/plugins/commentary.lua
@@ -0,0 +1,10 @@
+return {
+  "tpope/vim-commentary",
+  lazy = true,
+  -- keys = {
+  --   { "<leader>c", mode = { "n", "v" }, desc = "Comment" },
+  --   { "<leader>cc", mode = { "n", "v" }, desc = "Comment" },
+  --   { "<leader>cu", mode = { "n", "v" }, desc = "Uncomment" },
+  -- },
+}
+

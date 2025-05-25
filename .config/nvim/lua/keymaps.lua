839599146deccadb6f6d45acbb130ec6e187be4e
rcmx <4450944+rcmx@users.noreply.github.com>
2025-05-25 08:13:07 -0700

    vim - add buffer movements

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 
 vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 
+-- navigation
+vim.keymap.set('n', '<leader>bp', ':bprev<CR>', { desc = 'Goto Previous Buffer', silent = true })
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Goto Next Buffer', silent = true })
+

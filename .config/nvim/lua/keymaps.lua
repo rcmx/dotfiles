013318769d80e9fdb0d9c4c6f5978bca61de68e9
rcmx <4450944+rcmx@users.noreply.github.com>
2025-06-22 15:53:33 -0700

    vim - update code keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -10,12 +10,12 @@ vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Goto Next Buffer', sil
 vim.keymap.set('n', '<C-left>', ':bprev<CR>', { desc = 'Goto Previous Buffer', silent = true })
 vim.keymap.set('n', '<C-right>', ':bnext<CR>', { desc = 'Goto Next Buffer', silent = true })
 
--- -- navigation code
--- vim.keymap.set("n", "[[", "?{<CR>w99[{", { desc = 'Goto Previous {', noremap = true, silent = true })
--- vim.keymap.set("n", "]]", "j0[[%/{<CR>", { desc = 'Goto Next {', noremap = true, silent = true })
--- vim.keymap.set("n", "][", "/}<CR>b99]}", { desc = 'Goto End of function', noremap = true, silent = true })
--- vim.keymap.set("n", "[]", "k$][%?}<CR>", { desc = 'Goto Start of function', noremap = true, silent = true })
+-- navigation code
+vim.keymap.set("n", "]m", "]mzz", { noremap = true, silent = true })
+vim.keymap.set("n", "[m", "[mzz", { noremap = true, silent = true })
 
+--
+--
 -- quicklist
 vim.keymap.set("n", "<C-j>", ":cnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", ":cprev<CR>", { noremap = true, silent = true })

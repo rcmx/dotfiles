fbc1e3d58c374a60f424ee12ce792e3967cc0038
rcmx <4450944+rcmx@users.noreply.github.com>
2025-06-28 18:01:05 -0700

    vim - snacks vertical layout

--- .config/nvim/lua/plugins/tools/snacks.lua
+++ .config/nvim/lua/plugins/tools/snacks.lua
@@ -10,9 +10,20 @@ return {
         bigfile = { enabled = true },
         dashboard = { enabled = true },
         explorer = { enabled = true },
-        indent = { enabled = true },
+        indent = { enabled = false ,
+            -- indent = {
+            --     -- enabled = false,
+            --     only_scope = true,
+            -- },
+            -- -- chunk = {
+            -- --     enabled = true,
+            -- -- },
+        },
         input = { enabled = true },
         picker = { enabled = true ,
+
+            layout = { preset = "vertical" },
+
             notifier = { enabled = true },
             quickfile = { enabled = true },
             scope = { enabled = true },
@@ -79,7 +90,7 @@ return {
         -- LSP
         { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
         { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+        -- { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
         { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
         { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
         { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },

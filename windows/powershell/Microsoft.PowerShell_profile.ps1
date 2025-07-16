dcb550ed1481e6229d13fca7b133345a1c0e563a
rcmx <4450944+rcmx@users.noreply.github.com>
2025-07-15 18:56:04 -0700

    powershell - emit OSC7 current directory

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,15 @@ function glol {git log --graph --abbrev-commit --oneline --decorate}
 
 set-alias -name lg -value lazygit
 
+
+function prompt {
+    $p = $executionContext.SessionState.Path.CurrentLocation
+        $osc7 = ""
+        if ($p.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $p.ProviderPath -Replace "\\", "/"
+        $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
+    }
+    "${osc7}PS $p$('>' * ($nestedPromptLevel + 1)) ";
+}
+

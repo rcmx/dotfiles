88c4af83650bec867ed95f3c993e4f60d5a9cd30
rcmx222 <1225939082+rcmx222@users.noreply.github.com>
2026-04-24 08:46:51 -0700

    nvim - add parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,26 @@ return {
 
             ts.setup({})
 
+            ts.install({
+                "c_sharp",
+                "dockerfile",
+                "editorconfig",
+                "html",
+                "javascript",
+                "json",
+                "json5",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "powershell",
+                "svelte",
+                "tmux",
+                "typescript",
+                "vim",
+                "xml",
+                "yaml",
+            })
+
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function(args)
                     pcall(vim.treesitter.start, args.buf)
